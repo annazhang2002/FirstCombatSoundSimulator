107cd3ed5abc7031ff88146ba4292acbf6e0e426
annazhang2002 <annazhang2002@gmail.com>
2018-07-29 09:41:43 -0700

    fixed big errors

--- StarterProject/Shared/DeviceViewController.swift
+++ StarterProject/Shared/DeviceViewController.swift
@@ -12,9 +12,24 @@ import AVFoundation
 
 class DeviceViewController: UIViewController {
     
-    @IBOutlet weak var deviceStatus: UILabel!
-    @IBOutlet weak var headView: headViewController!
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let optionsViewController = segue.destination as?
+            OptionsViewController {
+            print("Preparing for OptionsViewController")
+            //optionsViewController.device = self.device
+        }
+    }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //set background image
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "camo.jpg")!)
+        
+    }
+    
+    //@IBOutlet weak var deviceStatus: UILabel!
+    //@IBOutlet weak var headView: headViewController!
     let PI : Double = 3.14159265359
     
     var device: MBLMetaWear!
@@ -27,6 +42,7 @@ class DeviceViewController: UIViewController {
     var seagullY : Float = 20
     var seagullZ : Float = -5
     
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated);
         
@@ -35,124 +51,123 @@ class DeviceViewController: UIViewController {
             self.device.led?.flashColorAsync(UIColor.green, withIntensity: 1.0, numberOfFlashes: 3)
             NSLog("We are connected")
         }
-        loadSounds()
-       
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+        //getDataFromSensor()
         
-        device.removeObserver(self, forKeyPath: "state")
-        device.led?.flashColorAsync(UIColor.red, withIntensity: 1.0, numberOfFlashes: 3)
-        device.disconnectAsync()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        /*
+         super.viewWillDisappear(animated)
+         
+         device.removeObserver(self, forKeyPath: "state")
+         device.led?.flashColorAsync(UIColor.red, withIntensity: 1.0, numberOfFlashes: 3)
+         device.disconnectAsync()
+         */
     }
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         OperationQueue.main.addOperation {
             switch (self.device.state) {
             case .connected:
-                self.deviceStatus.text = "Connected";
+                print("Connected");
                 self.device.sensorFusion?.mode = MBLSensorFusionMode.imuPlus
             case .connecting:
-                self.deviceStatus.text = "Connecting";
+                print("Connecting");
             case .disconnected:
-                self.deviceStatus.text = "Disconnected";
+                print("Disconnected");
             case .disconnecting:
-                self.deviceStatus.text = "Disconnecting";
+                print("Disconnecting");
             case .discovery:
-                self.deviceStatus.text = "Discovery";
+                print("Discovery");
             }
         }
     }
     
-    func getFusionValues(obj: MBLEulerAngleData){
-        
-        let xS =  String(format: "%.02f", (obj.p))
-        let yS =  String(format: "%.02f", (obj.y))
-        let zS =  String(format: "%.02f", (obj.r))
+    /*
+     func getDataFromSensor() {
+     device.sensorFusion?.eulerAngle.startNotificationsAsync { (obj, error) in
+     self.getFusionValues(obj: obj!)
+     }.success { result in
+     print("Successfully subscribed")
+     }.failure { error in
+     print("Error on subscribe: \(error)")
+     }
+     
+     }
+     
+     func getFusionValues(obj: MBLEulerAngleData){
+     
+     let xS =  String(format: "%.02f", (obj.p))
+     let yS =  String(format: "%.02f", (obj.y))
+     let zS =  String(format: "%.02f", (obj.r))
+     
+     let x = radians((obj.p * -1) + 90)
+     let y = radians(abs(365 - obj.y))
+     let z = radians(obj.r)
+     //headView.setPointerPosition(w: 0.0, x : x, y: y, z: z)
+     playSoundsController.updateAngularOrientation(abs(Float(365 - obj.y)))
+     // Send OSC here
+     }
+     
+     
+     func radians(_ degree: Double) -> Double {
+     return ( PI/180 * degree)
+     }
+     func degrees(_ radian: Double) -> Double {
+     return (180 * radian / PI)
+     }
+     
+     
+     func loadSounds(){
+     var soundArray : [String] = []
+     for index in 0...3{
+     soundArray.append(String(index) + ".wav")
+     }
+     playSoundsController = PlaySoundsController(file: soundArray)
+     
+     playSoundsController.updatePosition(index: 0, position: AVAudio3DPoint(x: 0, y: 0, z: -15))
+     playSoundsController.updatePosition(index: 1, position: AVAudio3DPoint(x: 7.5, y: 10, z: 7.5 * sqrt(2.0)))
+     playSoundsController.updatePosition(index: 2, position: AVAudio3DPoint(x: 0, y: -2, z: 0))
+     playSoundsController.updatePosition(index: 3, position: AVAudio3DPoint(x: -100, y: 10, z: -5))
+     
+     for sounds in soundArray.enumerated(){
+     // skip seagguls
+     if sounds.offset != 3 {
+     playSoundsController.play(index: sounds.offset)
+     }
+     }
+     
+     }
+     
+     
+     @IBAction func seagulls(_ sender: UIButton) {
+     timer = Timer()
+     //startTime = TimeInterval()
+     let aSelector : Selector = #selector(self.moveSoundsLinearPath)
+     timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: aSelector,     userInfo: nil, repeats: true)
+     //startTime = Date.timeIntervalSinceReferenceDate
+     //play seagulls here
+     playSoundsController.play(index: 3)
+     }
+     
+     func moveSoundsLinearPath(){
+     
+     playSoundsController.updatePosition(index: 3, position: AVAudio3DPoint(x: seagullX, y: seagullY, z: seagullZ))
+     seagullX += 0.1
+     if seagullX > 100.0 {
+     playSoundsController.stop(index: 3)
+     stopTimer()
+     seagullX = -100
+     }
+     }
+     
+     
+     func stopTimer() {
+     if timer != nil {
+     timer?.invalidate()
+     timer = nil
+     }
+     }
+     */
     
-        let x = radians((obj.p * -1) + 90)
-        let y = radians(abs(365 - obj.y))
-        let z = radians(obj.r)
-        headView.setPointerPosition(w: 0.0, x : x, y: y, z: z)
-        playSoundsController.updateAngularOrientation(abs(Float(365 - obj.y)))
-        
-        // Send OSC here
-    }
- 
-    func radians(_ degree: Double) -> Double {
-        return ( PI/180 * degree)
-    }
-    func degrees(_ radian: Double) -> Double {
-        return (180 * radian / PI)
-    }
-    
-    
-    @IBAction func startPressed(sender: AnyObject) {
-        
-        device.sensorFusion?.eulerAngle.startNotificationsAsync { (obj, error) in
-            self.getFusionValues(obj: obj!)
-            }.success { result in
-                print("Successfully subscribed")
-            }.failure { error in
-                print("Error on subscribe: \(error)")
-        }
-    }
-    
-    @IBAction func stopPressed(sender: AnyObject) {
-        device.sensorFusion?.eulerAngle.stopNotificationsAsync().success { result in
-            print("Successfully unsubscribed")
-            }.failure { error in
-                print("Error on unsubscribe: \(error)")
-        }
-    }
-    
-    func loadSounds(){
-        var soundArray : [String] = []
-        for index in 0...3{
-            soundArray.append(String(index) + ".wav")
-        }
-        playSoundsController = PlaySoundsController(files: soundArray)
-        
-        playSoundsController.updatePosition(index: 0, position: AVAudio3DPoint(x: 0, y: 0, z: -15))
-        playSoundsController.updatePosition(index: 1, position: AVAudio3DPoint(x: 7.5, y: 10, z: 7.5 * sqrt(2.0)))
-        playSoundsController.updatePosition(index: 2, position: AVAudio3DPoint(x: 0, y: -2, z: 0))
-        playSoundsController.updatePosition(index: 3, position: AVAudio3DPoint(x: -100, y: 10, z: -5))
-        
-        for sounds in soundArray.enumerated(){
-            // skip seagguls
-            if sounds.offset != 3 {
-                playSoundsController.play(index: sounds.offset)
-            }
-        }
-    
-    }
-    
-    @IBAction func seagulls(_ sender: UIButton) {
-        timer = Timer()
-        //startTime = TimeInterval()
-        let aSelector : Selector = #selector(self.moveSoundsLinearPath)
-        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: aSelector,     userInfo: nil, repeats: true)
-        //startTime = Date.timeIntervalSinceReferenceDate
-        //play seagulls here
-        playSoundsController.play(index: 3)
-    }
-    
-    func moveSoundsLinearPath(){
-        
-        playSoundsController.updatePosition(index: 3, position: AVAudio3DPoint(x: seagullX, y: seagullY, z: seagullZ))
-        seagullX += 0.1
-        if seagullX > 100.0 {
-            playSoundsController.stop(index: 3)
-            stopTimer()
-            seagullX = -100
-        }
-    }
-    
-    func stopTimer() {
-        if timer != nil {
-            timer?.invalidate()
-            timer = nil
-        }
-    }
 }
--- StarterProject/Shared/RunViewController.swift
+++ StarterProject/Shared/RunViewController.swift
@@ -8,11 +8,106 @@
 
 import UIKit
 import AVFoundation
-
-class RunViewController: UIViewController {
+import MetaWear
+class RunViewController: UIViewController, SendScenarioDelegate {
     
-    var soundPlayer: PlaySoundsController? = nil
+    var scene: Int?
+    var soundArray : [String] = []
     
+    
+    var optionsViewController: OptionsViewController?
+    
+    func sendScenario(scene: Int) {
+        self.scene = scene
+        print("Scenario: " + String(scene))
+    }
+    
+    var playSoundsController: PlaySoundsController? = nil
+    
+    @IBOutlet weak var deviceStatus: UILabel!
+    
+    let PI : Double = 3.14159265359
+    
+    var device: MBLMetaWear!
+    
+    
+    func data (p: Float, y : Float, r: Float) {
+        print("p: " + String(p))
+        
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        
+        /*
+         device.addObserver(self, forKeyPath: "state", options: NSKeyValueObservingOptions.new, context: nil)
+         device.connectAsync().success { _ in
+         self.device.led?.flashColorAsync(UIColor.green, withIntensity: 1.0, numberOfFlashes: 3)
+         NSLog("We are connected")
+         }
+         */
+        loadSounds()
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        //disconnect the sensors when the view disappears
+        device.removeObserver(self, forKeyPath: "state")
+        device.led?.flashColorAsync(UIColor.red, withIntensity: 1.0, numberOfFlashes: 3)
+        device.disconnectAsync()
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        OperationQueue.main.addOperation {
+            switch (self.device.state) {
+            case .connected:
+                self.deviceStatus.text = "Connected";
+                self.device.sensorFusion?.mode = MBLSensorFusionMode.imuPlus
+                print("connected")
+                
+            case .connecting:
+                self.deviceStatus.text = "Connecting";
+                print("connecting")
+            case .disconnected:
+                self.deviceStatus.text = "Disconnected";
+                print("Disconnected")
+            case .disconnecting:
+                self.deviceStatus.text = "Disconnecting";
+                print("Disconnecting")
+            case .discovery:
+                self.deviceStatus.text = "Discovery";
+                print("Discovery")
+            }
+        }
+    }
+    
+    func getFusionValues(obj: MBLEulerAngleData){
+        
+        let xS =  String(format: "%.02f", (obj.p))
+        let yS =  String(format: "%.02f", (obj.y))
+        let zS =  String(format: "%.02f", (obj.r))
+        
+        let x = radians((obj.p * -1) + 90)
+        let y = radians(abs(365 - obj.y))
+        let z = radians(obj.r)
+        
+        print("X: " + String(x))
+        print("Y: " + String(y))
+        print("Z: " + String(z))
+        playSoundsController?.updateAngularOrientation(abs(Float(365 - obj.y)))
+        
+        // Send OSC here
+    }
+    
+    func radians(_ degree: Double) -> Double {
+        return ( PI/180 * degree)
+    }
+    func degrees(_ radian: Double) -> Double {
+        return (180 * radian / PI)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,50 +116,77 @@ class RunViewController: UIViewController {
         //set background image
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "camo.jpg")!)
         
+        optionsViewController?.scenarioSender = self
     }
     
     @IBAction func startPressed(_ sender: UIButton) {
-        loadSounds()
+        device.sensorFusion?.eulerAngle.startNotificationsAsync { (obj, error) in
+            self.getFusionValues(obj: obj!)
+            }.success { result in
+                print("Successfully subscribed")
+            }.failure { error in
+                print("Error on subscribe: \(error)")
+        }
+        
+        //plays the sounds in the array
+        for sound in soundArray.enumerated() {
+            playSoundsController?.play(index: sound.offset)
+        }
         
     }
     
     @IBAction func stopPressed(_ sender: UIButton) {
         
+        device.sensorFusion?.eulerAngle.stopNotificationsAsync().success { result in
+            print("Successfully unsubscribed")
+            }.failure { error in
+                print("Error on unsubscribe: \(error)")
+        }
+        
+        //plays the sounds in the array
+        for sound in soundArray.enumerated() {
+            playSoundsController?.stop(index: sound.offset)
+        }
         
     }
     
     
     func loadSounds() {
         
-        //adding the sound files to an array
-        var soundArray : [String] = []
-        
         /* sounds
-         * 0.wav : sandyRockRunning
-         * 1.wav : gunShots
+         * 0.wav : AK47
+         * 1.wav : Explosion 2
+         * 2.wav : Explosion
+         * 3.wav : Grenade Launcher
+         * 4.wav : Helicopter 2
+         * 5.wav : Helicopter Flyby
+         * 6.wav : Helicopter
+         * 7.wav : Machine Gun 1
+         * 8.wav : Machine Gun 2
+         * 9.wav : Machine Gun 3
+         * 10.wav : Machine Gun 4
+         * 11.wav : Overal Battle
+         * 12.wav : Shotgun
+         * 13.wav : Sniper Rifle
+         * 14.wav : Tank
          */
         
         
-        for index in 0...0 {
+        for index in 0...14 {
             soundArray.append(String(index) + ".wav")
         }
         
         
         //initializing the player with the soundArray files
-        soundPlayer = PlaySoundsController(files: soundArray)
+        playSoundsController = PlaySoundsController(files: soundArray)
         
         //put the sounds into specific positions
-        soundPlayer?.updatePosition(index: 0, position: AVAudio3DPoint(x: 50, y: 50, z: 0))
-        
-        
-        //plays the sounds in the array
-        for sound in soundArray.enumerated() {
-            soundPlayer?.play(index: sound.offset)
-        }
-        
-        
+        playSoundsController?.updatePosition(index: 0, position: AVAudio3DPoint(x: 50, y: 50, z: 0))
         
     }
     
+    func sendScenario(scenario: Int) {
+        print(scenario)
+    }
     
 }
